c6d372a694cb0e0dab2b8591e31c0c4ee9c355c8
Phil Lembo <phil.lembo@gmail.com>
2020-05-08 17:29:23 -0400

    Update to use connect-flash and express-session middleware

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var session = require('express-session');
+var flash = require('connect-flash');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,6 +10,35 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Session middleware
+// NOTE: Uses default in-memory session store, which is not
+// suitable for production
+app.use(session({
+  secret: 'your_secret_value_here',
+  resave: false,
+  saveUninitialized: false,
+  unset: 'destroy'
+}));
+
+// Flash middleware
+app.use(flash());
+
+// Set up local vars for template layout
+app.use(function(req, res, next) {
+  // Read any flashed errors and save
+  // in the response locals
+  res.locals.error = req.flash('error_msg');
+
+  // Check for simple error string and
+  // convert to layout's expected format
+  var errs = req.flash('error');
+  for (var i in errs){
+    res.locals.error.push({message: 'An error occurred', debug: errs[i]});
+  }
+
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
