25581759588d17c80381582b38a96e2410d11130
Phil Lembo <phil.lembo@gmail.com>
2020-05-08 18:06:48 -0400

    Wraps tokens in an AccessToken object

--- app.js
+++ app.js
@@ -30,6 +30,19 @@ passport.deserializeUser(function(id, done) {
   done(null, users[id]);
 });
 
+// Configure simple-oauth2
+const oauth2 = require('simple-oauth2').create({
+  client: {
+    id: process.env.OAUTH_APP_ID,
+    secret: process.env.OAUTH_APP_PASSWORD
+  },
+  auth: {
+    tokenHost: process.env.OAUTH_AUTHORITY,
+    authorizePath: process.env.OAUTH_AUTHORIZE_ENDPOINT,
+    tokenPath: process.env.OAUTH_TOKEN_ENDPOINT
+  }
+});
+
 // Callback function called once the sign-in is complete
 // and an access token has been obtained
 async function signInComplete(iss, sub, profile, accessToken, refreshToken, params, done) {
