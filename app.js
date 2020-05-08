2c0b499e77ff97d72f980f0961133922c6abea67
Phil Lembo <phil.lembo@gmail.com>
2020-05-08 18:18:24 -0400

    Use the calendar route

--- app.js
+++ app.js
@@ -146,6 +146,7 @@ app.use(function(req, res, next) {
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
+app.use('/calendar', calendarRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
