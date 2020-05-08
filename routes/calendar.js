f453ebabc11aedb8c595d5c1c6a27dfd734f5285
Phil Lembo <phil.lembo@gmail.com>
2020-05-08 18:15:58 -0400

    New route for calendar data

---
+++ routes/calendar.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var tokens = require('../tokens.js');
+var graph = require('../graph.js');
+
+/* GET /calendar */
+router.get('/',
+  async function(req, res) {
+    if (!req.isAuthenticated()) {
+      // Redirect unauthenticated requests to home page
+      res.redirect('/')
+    } else {
+      let params = {
+        active: { calendar: true }
+      };
+
+      // Get the access token
+      var accessToken;
+      try {
+        accessToken = await tokens.getAccessToken(req);
+      } catch (err) {
+        res.json(err);
+      }
+
+      if (accessToken && accessToken.length > 0) {
+        try {
+          // Get the events
+          var events = await graph.getEvents(accessToken);
+
+          res.json(events.value);
+        } catch (err) {
+          res.json(err);
+        }
+      }
+      else {
+        req.flash('error_msg', 'Could not get an access token');
+      }
+    }
+  }
+);
+
+module.exports = router;
